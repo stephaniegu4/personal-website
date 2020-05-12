4b8069fb6890de602e8cee13dd45ead09b8f0c69
Stephanie Gu <stephaniegu7h@gmail.com>
2020-05-11 23:00:20 -0400

    Added navigation bar
    All nav items route to home page currently

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
+import NavBar from './navbar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <NavBar/>
+    <Switch>
+
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ src/navbar.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { 
+    Collapse,
+    Navbar, 
+    NavbarToggler,
+    NavLink,
+    NavItem,
+    NavbarBrand,
+    Nav 
+} from 'reactstrap';
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        var containerStyle = {
+            width: '80%',
+            display:'block',
+            marginLeft:'auto',
+            marginRight:'auto',
+        }
+
+        var navItemStyle = {
+            paddingLeft:'10px'
+        }
+
+        var navBrandStyle = {
+            color:'#eba7d0'
+        }
+
+        var navStyle = {
+            float:'right'
+        }
+
+        return(
+            <div style={containerStyle}>
+                <Navbar color="faded" light expand="md">
+                    <NavbarBrand href="/" style={navBrandStyle}>steph gu</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem style={navItemStyle}>
+                                <NavLink href="/">About</NavLink>
+                            </NavItem>
+                            <NavItem style={navItemStyle}>
+                                <NavLink href="/">Gallery</NavLink>
+                            </NavItem>
+                            <NavItem style={navItemStyle}>
+                                <NavLink href="/">Experience</NavLink>
+                            </NavItem>
+                            <NavItem style={navItemStyle}>
+                                <NavLink href="/">Goals</NavLink>
+                            </NavItem>
+                            <NavItem style={navItemStyle}>
+                                <NavLink href="/">Contact</NavLink>
+                            </NavItem>
+                            <NavItem style={navItemStyle}>
+                                <NavLink href="/">Resume</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
