dc644daae9aeaaab635fb455461087801bc205b3
Stephanie Gu <stephaniegu7h@gmail.com>
2020-05-17 15:25:52 -0400

    Added re render on window resize debounced

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
 import NavBar from './navbar.js';
@@ -10,14 +9,55 @@ import Home from './home.js';
 import About from './about.js';
 import ContactSection from './contact.js';
 
+function debounce(value, delay) {
+  let timer
+
+  return () => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      timer = null
+      value.apply(this, arguments)
+    }, delay)
+  }
+}
+ 
+function App() {
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+
+  
+
+  useEffect(() => {
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+    }, 1000)
+
+    window.addEventListener('resize', debouncedHandleResize)
+
+    // cleanup so handleResize is not binded multiple times 
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize)
+    }
+  })
+
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/contact" component={ContactSection} />
+      </Switch>
+    </Router>
+  );
+}
+
 ReactDOM.render(
-  <Router>
-    <NavBar/>
-    <Switch>
-      <Route path="/" component={Home} exact/>
-      <Route path="/contact" component={ContactSection} />
-    </Switch>
-  </Router>,
+  <App />,
   document.getElementById('root')
 );
 
